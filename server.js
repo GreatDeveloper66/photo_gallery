e6a43eba470f7fb790c93dcb24736f47f3858c47
Adam Shaffer <adam.shaffer@fdmgroup.com>
2021-04-28 19:05:16 -0500

    launch react client through port 5000 from backend

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
+import bodyparser from 'body-parser'
 
 const app = express()
 
-app.get('/', (req,res) => res.send('Hello World'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
+app.use(express.static(path.join(path.resolve(), "client","build")))
+
+//app.get('/', (req,res) => res.send('Hello World'))
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(path.resolve(), "client", "build", "index.html"))
+})
 const port = process.env.PORT
 app.listen(port, () => console.log(`listening on PORT: ${port}`))
