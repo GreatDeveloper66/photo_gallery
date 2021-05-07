4ca121fd1651a6f7cea74c255a57c74c14619261
Adam Shaffer <adam.shaffer@fdmgroup.com>
2021-05-06 19:09:12 -0500

    Add handleRandomPhotos method

--- client/src/Components/GalleryScreenComponents/MainScreen.js
+++ client/src/Components/GalleryScreenComponents/MainScreen.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react"
+import { Button } from 'reactstrap'
 
 class MainScreen extends Component {
+
+  constructor(props) {
+    super()
+    this.handleRandomPhotos = this.handleRandomPhotos.bind(this)
+  }
+
+  handleRandomPhotos() {
+    console.log('this')
+  }
+
   render() {
     return (
       <div>
+        <Button onClick = {this.handleRandomPhotos}>
+          Random Photos
+        </Button>
         WASSSSUPPP!
       </div>
     );
