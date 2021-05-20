4e9348451b1b31a9ed6e45de5393525864aad522
Adam Shaffer <adam.shaffer@fdmgroup.com>
2021-05-20 17:37:51 -0500

    Fetch through API to unspash successful

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import unsplash from 'unsplash-js'
 dotenv.config()
 import path from 'path'
 import bodyparser from 'body-parser'
+import nodeFetch from 'node-fetch';
 
 const app = express()
 
@@ -14,6 +16,21 @@ app.use(bodyparser.json())
 app.use(express.static(path.join(path.resolve(), "client","build")))
 
 //app.get('/', (req,res) => res.send('Hello World'))
+const serverApi = unsplash.createApi({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY,
+  apiUrl: process.env.APIURL,
+  fetch: nodeFetch
+})
+
+serverApi.search.getPhotos({
+  query: 'cat',
+  page: 1,
+  perPage: 10,
+  color: 'green',
+  orientation: 'portrait',
+}).then(result => console.log(result.response));
+
+
 
 app.get('*', (req,res) => {
   res.sendFile(path.join(path.resolve(), "client", "build", "index.html"))
