89d7407e9ba5f92aa461051e802b09a9d6eab5c4
Adam Shaffer <adam.shaffer@fdmgroup.com>
2021-05-21 18:33:07 -0500

    Add front end call to cats route

--- client/src/Actions/ActionDispatchers/LoadRandomPhotosActionDispatcher.js
+++ client/src/Actions/ActionDispatchers/LoadRandomPhotosActionDispatcher.js
@@ -4,7 +4,10 @@ function LoadRandomPhotosActionDispatcher() {
   return dispatch => {
     setTimeout(() => {
       //make fetch request here to get jwt
-      const photos = 'photos'
+      let photos = []
+      fetch(`${process.env.REACT_APP_ROUTE}${process.env.REACT_APP_CATS}`)
+        .then(resp => resp.json())
+        .then(data => photos = data)
       dispatch(LoadRandomPhotosActionCreator(photos))
     }, 1000)
   }
