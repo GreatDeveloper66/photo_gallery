53f8f7c27bda963019651071ef451e2bb8dfbf0a
Adam Shaffer <adam.shaffer@fdmgroup.com>
2021-06-13 16:52:53 -0500

    Add Favorite Pic Model

--- models/FavoritePic.js
+++ models/FavoritePic.js
@@ -3,8 +3,13 @@ import * as mongoose from "mongoose";
 const Schema = mongoose.Schema
 const ObjectId = Schema.ObjectId
 
-const FavoritePic = new Schema({
+const FavoritePicSchema = new Schema({
     id: ObjectId,
     picId: ObjectId,
     userId: ObjectId
 })
+
+const FavoritePicModel = mongoose.model('FavoritePic',FavoritePicSchema)
+
+
+export default FavoritePicModel
